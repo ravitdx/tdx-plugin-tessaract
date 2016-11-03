002c4139370153c4faa2273785474897b88e8022
Ravi <teja@believecreative.com>
2016-11-03 17:49:12 +0530

    new

--- www/tesseractPlugin.js
+++
@@ -1,9 +0,0 @@
-var TesseractPlugin = {
-    recognizeText: function (image, language, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "TesseractPlugin", "recognizeText", [language, image]);
-    },
-    loadLanguage: function (language, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "TesseractPlugin", "loadLanguage", [language]);
-    }
-};
-module.exports = TesseractPlugin;
